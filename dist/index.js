f0f24ab56db54477b44e76ade97080ea4f0485b0
ethutami uta <ethutami@gmail.com>
2025-08-24 19:22:34 +0800

    fixed missing file

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const config_1 = require("./config");
+const port = config_1.PORT || 8010;
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)({
+    // origin: 'https://ethika-utami.vercel.app'
+    origin: config_1.FE_URL
+}));
+app.use((0, cors_1.default)());
+app.get('/', (req, res) => {
+    res.status(200).json('Welcome to Conferency api');
+});
+app.use((err, req, res, next) => {
+    res.status(400).json({
+        success: false,
+        message: err.message,
+    });
+});
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
