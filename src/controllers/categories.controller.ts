42c8fed634eb43480941893e9af8c10424778abd
ethutami uta <ethutami@gmail.com>
2025-09-05 15:35:42 +0800

    add Icon url  storage

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,13 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import { getAllCategoriesService } from "../services/categories.service";
 
-export async function GetAllCategoryController(req: Request, res: Response, next: NextFunction
-) {
+export async function GetAllCategoryController(req: Request, res: Response, next: NextFunction) {
     try {
         const categories = await getAllCategoriesService();
+
+        const supabaseStorageUrl = 'https://mznyfinmwapfnwytaooe.supabase.co/storage/v1/object/public/';
+
+        const categoriesWithIconUrl = categories.map(category => ({
+            ...category,
+            icon: supabaseStorageUrl + category.icon
+        }));
+
         res.status(200).send({
+            status: 'success',
             message: `Get all categories success`,
-            data: categories,
+            data: categoriesWithIconUrl,
         });
     } catch (err) {
         next(err);
